3fb9dd793fe88235b3cd7130e5fbd2432599adab
james <illuillu55@hotmail.com>
2018-07-04 21:56:23 +0900

    0704

--- board/views.py
+++ board/views.py
@@ -19,13 +19,11 @@ def list(request):
     return render(request, 'board/list.html', context)
 
 def add(request):
-    result = User.objects.filter(name=request.POST['name'])
-    print("result=========", result[0])
     board = Board()
     board.title = request.POST['title']
     board.content = request.POST['content']
     board.hit += 1
-    board.user = result[0]
+    board.user = request.session['authuser']['name']
 
     board.save()
     return HttpResponseRedirect('/board')
